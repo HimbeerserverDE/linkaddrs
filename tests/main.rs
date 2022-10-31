68cb196e010836a943f20d8dade7930aee2efaf4
HimbeerserverDE <himbeerserverde@gmail.com>
2022-10-31 17:24:55 +0100

    test filtering for all v4/v6

---
+++ tests/main.rs
@@ -0,0 +1,49 @@
+use linkaddrs::Result;
+
+#[test]
+fn test_addresses() -> Result<()> {
+    let addrs = linkaddrs::addresses(String::from("lo"))?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
+
+#[test]
+fn test_ipv4_addresses() -> Result<()> {
+    let addrs = linkaddrs::ipv4_addresses(String::from("lo"))?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
+
+#[test]
+fn test_ipv6_addresses() -> Result<()> {
+    let addrs = linkaddrs::ipv6_addresses(String::from("lo"))?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
+
+#[test]
+fn test_all_addresses() -> Result<()> {
+    let addrs = linkaddrs::all_addresses()?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
+
+#[test]
+fn test_all_ipv4_addresses() -> Result<()> {
+    let addrs = linkaddrs::all_ipv4_addresses()?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
+
+#[test]
+fn test_all_ipv6_addresses() -> Result<()> {
+    let addrs = linkaddrs::all_ipv6_addresses()?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
