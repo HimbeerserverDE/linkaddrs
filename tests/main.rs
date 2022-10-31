1cd3fc2be8baa6b84896cb6731a102539f2e66b9
HimbeerserverDE <himbeerserverde@gmail.com>
2022-10-31 11:13:24 +0100

    test filtering for IPv6

--- tests/main.rs
+++ tests/main.rs
@@ -17,6 +17,14 @@ fn test_ipv4_addresses() -> Result<()> {
 }
 
 #[test]
+fn test_ipv6_addresses() -> Result<()> {
+    let addrs = linkaddrs::ipv6_addresses(String::from("lo"))?;
+    println!("{:?}", addrs);
+
+    Ok(())
+}
+
+#[test]
 fn test_all_addresses() -> Result<()> {
     let addrs = linkaddrs::all_addresses()?;
     println!("{:?}", addrs);
